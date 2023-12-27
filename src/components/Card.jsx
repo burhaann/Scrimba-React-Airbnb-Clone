0d8bd92f983f281b7caef2062b00a1912c4c8f0b
Burhaann <burhaann321@gmail.com>
2023-12-27 12:30:51 +0530

    Done with Part 2 of Scrimba React Course, Finished Air BNB Clone

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 export default function Card(props) {
   let badgeText;
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
-        src={`src/assets/images/${props.item.coverImg}`}
+        src={`src/assets/images/${props.coverImg}`}
         className="card--image"
       />
       <div className="card--stats">
         <img src="src/assets/images/star.png" className="card--star" />
-        <span>{props.item.stats.rating}</span>
-        <span className="gray">({props.item.stats.reviewCount}) • </span>
-        <span className="gray">{props.item.location}</span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
-      <p>{props.item.title}</p>
+      <p>{props.title}</p>
       <p>
-        <span className="bold">From ${props.item.price}</span> / person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
