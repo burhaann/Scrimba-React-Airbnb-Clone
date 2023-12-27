fbfb74c9c25d0f2848886e07c63cb2c3d1e80a24
Burhaann <burhaann321@gmail.com>
2023-12-27 12:26:58 +0530

    Done with Part 2 of Scrimba React Course, Finished Air BNB Clone

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="src/assets/images/airbnb-logo.png"
+        alt="airbnb-logo"
+        className="nav--logo"
+      />
+    </nav>
+  );
+}
