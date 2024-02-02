7da17315f733b49e72cb0344e61a47e7676c4c69
Burhaann <burhaann321@gmail.com>
2024-02-02 15:48:03 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="hero">
-      <img src="src/assets/images/photo-grid.png" className="hero--photo" />
+      <img
+        src="src/assets/images/photo-grid.png"
+        className="hero--photo"
+        alt="photo-grid"
+      />
       <h1 className="hero--header">Online Experiences</h1>
       <p className="hero--text">
         Join unique interactive activities led by one-of-a-kind hosts—all
